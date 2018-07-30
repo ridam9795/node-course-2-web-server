dfacb619f14064fd1636dc55723712085648bdc0
rid <ridamnagar11@gmail.com>
2018-07-30 13:59:34 +0530

    intial commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const express=require('express');
+const hbs=require('hbs');
+const fs=require('fs');
+var app=express();
+app.set('view engine','hbs');
+hbs.registerPartials(__dirname + '/views/partials')
+app.use(express.static('D:/Nodejs/new-web-server/public'));
+app.use((req,res,next)=>{
+         var now=new Date().toString();
+         var log=`${now}${req.method}${req.url}`;
+         console.log(log);
+         fs.appendFile('server.log',log + '/n',(error)=>{
+             console.log(error);
+         })
+         next();
+})
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear();
+})
+hbs.registerHelper('toUpperCase',(text)=>{
+    return text.toUpperCase();
+})
+
+app.use((req,res,next)=>{
+res.render('maintainance.hbs',{
+    heading:"We will right back",
+    paragarph:"Site is being updated"
+});
+});
+app.get('/',(req,res)=>{
+    res.render('home.hbs',{
+        welcomeMessage:"welcome to my website",
+        name:"Ridam",
+    });
+    });
+app.get('/about',(req,res)=>{
+res.render('about.hbs',{
+    welcomeMessage:"welcome to my website",
+    heading:"About page",
+})
+});
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+  });
+  
